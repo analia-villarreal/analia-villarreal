a3285e5dbfc249c2c73b760335cb14289791d938
Analia <ana.lau.villarreal@gmail.com>
2022-05-23 19:11:49 -0300

    funciona el boton

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 
-navToggle.addEventListener("Click",()=>{navMenu.classList.toggle("nav-menu_visible");});
+navToggle.addEventListener("click",()=>{navMenu.classList.toggle("nav-menu_visible");});
 
-navToggle.addEventListener("Click",()=> )
+
